86dacf3d3e47dc0eda81d869ca1e78e6d0492e86
Sunny Dhama <sunnydhama@yahoo.com>
2020-07-25 20:45:14 +0530

    added auth navigation logic

--- src/Routes.js
+++ src/Routes.js
@@ -1,22 +1,47 @@
+import { useAuth } from 'contexts/auth';
 import React from 'react';
-import { Switch, Redirect } from 'react-router-dom';
-
+import { Redirect, Switch } from 'react-router-dom';
 import { RouteWithLayout } from './components';
-import { Main as MainLayout} from './layouts';
-
+import { Main as MainLayout, Minimal as MinimalLayout } from './layouts';
 import {
   Dashboard as DashboardView,
   ProductList as ProductListView,
+  SignIn as SignInView,
+  SignUp as SignUpView,
+  NotFound as NotFoundView
 } from './views';
 
 const Routes = () => {
+  const { isAuthenticated } = useAuth();
+  if (!isAuthenticated) {
+    return (
+      <Switch>
+        <Redirect exact from="/" to="/sign-in" />
+        <RouteWithLayout
+          component={SignUpView}
+          exact
+          layout={MinimalLayout}
+          path="/sign-up"
+        />
+        <RouteWithLayout
+          component={SignInView}
+          exact
+          layout={MinimalLayout}
+          path="/sign-in"
+        />
+        <RouteWithLayout
+          component={NotFoundView}
+          exact
+          layout={MinimalLayout}
+          path="/not-found"
+        />
+        <Redirect to="/not-found" />
+      </Switch>
+    );
+  }
   return (
     <Switch>
-      <Redirect
-        exact
-        from="/"
-        to="/dashboard"
-      />
+      <Redirect exact from="/" to="/dashboard" />
       <RouteWithLayout
         component={DashboardView}
         exact
@@ -29,6 +54,13 @@ const Routes = () => {
         layout={MainLayout}
         path="/products"
       />
+      <RouteWithLayout
+        component={NotFoundView}
+        exact
+        layout={MinimalLayout}
+        path="/not-found"
+      />
+      <Redirect to="/not-found" />
     </Switch>
   );
 };
