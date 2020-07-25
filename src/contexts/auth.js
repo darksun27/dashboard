f1ac5b025fdcd04ea93d1752d06fe47353dc2218
Sunny Dhama <sunnydhama@yahoo.com>
2020-07-26 00:18:16 +0530

    added signin logout

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -10,10 +10,15 @@ export const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     let user = JSON.parse(localStorage.getItem('user'));
+    if (!user) return;
     let { token = null } = user;
     if (token) setisAuthenticated(true);
-    else setisAuthenticated(false);
   }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    setisAuthenticated(false);
+  };
 
   const signUp = async ({ email, password }) => {
     setisLoading(true);
@@ -46,15 +51,35 @@ export const AuthProvider = ({ children }) => {
       return false;
     }
   };
-  const signIn = async () => {
+  const signIn = async ({ email, password }) => {
     setisLoading(true);
     try {
-      let { data } = await new Axios({
+      let data = new FormData();
+      data.append('email', email);
+      data.append('password', password);
+      let {
+        data: { token, type }
+      } = await new Axios({
         method: 'POST',
-        url: `${BASE_URI}/signup`
+        url: `${BASE_URI}/signin`,
+        config: { headers: { 'Content-Type': 'multipart/form-data' } },
+        data
       });
+      localStorage.setItem(
+        'user',
+        JSON.stringify({
+          token,
+          type
+        })
+      );
+      console.log(type);
+      console.log('SignIn success');
+      setisAuthenticated(true);
+      return true;
     } catch (e) {
+      console.log('SignIn Failed');
       console.log(e);
+      return false;
     }
   };
   return (
@@ -64,7 +89,8 @@ export const AuthProvider = ({ children }) => {
         setisAuthenticated,
         signUp,
         signIn,
-        isLoading
+        isLoading,
+        logout
       }}>
       {children}
     </AuthContext.Provider>
